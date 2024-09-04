19d1476ac5dee76d808028aecc67d39ed91704b1
payas <sehzade172o41@gmail.com>
2024-09-04 20:06:59 +0300

    ğ

--- RC-10-lifeCyle/src/Clock.jsx
+++ RC-10-lifeCyle/src/Clock.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Clock = () => {
-  return (
-    <div>Clock</div>
-  )
-}
+  return <div>Clock</div>;
+};
 
-export default Clock
+export default Clock;
