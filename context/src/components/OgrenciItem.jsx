e5a5af3b08692bb241674917842cecbdd8b7fbe1
payas <sehzade172041@gmail.com>
2024-09-12 17:40:52 +0300

    qw

--- context/src/components/OgrenciItem.jsx
+++ context/src/components/OgrenciItem.jsx
@@ -1,22 +1,24 @@
 import React from "react";
 import { useContext } from "react";
-import { StudentContext } from "react";
+import { StudentContext } from "../App";
 
 const OgrenciItem = () => {
-  const { student, changeColor } = useContext(StudentContext);
+  //! 3 (context consumer) globalde (mesela App.js yada ayrı bir context sayfası) tanımladığım değerleri buraya çağırdık
+
+  const { students, changeColor } = useContext(StudentContext);
 
   return (
     <div>
-      {student.map((std) => (
+      {students.map((std) => (
         <div key={std.id} style={{ background: std.color }}>
           <h3>NAME: {std.name}</h3>
-          <h4>EMAIL: {std.email}</h4>
-          <h4>AGE: {std.age}</h4>
-          COLOR:{" "}
+          <h4>EMAIL: {std.email} </h4>
+          <h4>AGE: {std.age} </h4>
+          Color:{" "}
           <input
             type="text"
             value={std.color}
-            onchange={(e) => changeColor(std.id, e.target.value)}
+            onChange={(e) => changeColor(std.id, e.target.value)}
           />
         </div>
       ))}
