d4117671f4c0a95725264bb0774c7fc14da7f3f3
payas <sehzade172041@gmail.com>
2024-09-12 17:49:11 +0300

    son

--- context/src/components/OgrenciList.jsx
+++ context/src/components/OgrenciList.jsx
@@ -4,11 +4,14 @@ import OgrenciItem from "./OgrenciItem";
 
 const OgrenciList = () => {
   return (
-    <div>
-      <img
+    <div >
+      <div style={{ textAlign:"center"}}>
+         <img 
         src="https://gelecekegitimde.com/wp-content/uploads/2015/01/fft99_mf3019637.jpeg"
         alt=""
       />
+      </div>
+     
 
       <h2 style={{ textAlign: "center", background: "pink" }}>
         ÖĞRENCİ LİSTESİ
