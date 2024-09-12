13aec6037655ece5aec5a1eb338bca8ba2716e91
payas <sehzade172041@gmail.com>
2024-09-12 17:13:25 +0300

    re

--- context/src/App.js
+++ context/src/App.js
@@ -1,10 +1,26 @@
+import { useState } from "react";
+import { createContext } from "react";
+import React from "react";
+import data from "./data";
+import Home from "./components/Home";
 
-function App() {
+export const StudentContext = createContext();
+
+const App = () => {
+  const [student, setStudent] = useState(data);
+  const changeColor = (id, newRenk) => {
+    setStudent(
+      student.map((a) => (a.id === id ? { ...a, color: newRenk } : a))
+    );
+  };
+
   return (
     <div className="App">
-   
+      <StudentContext.Provider value={{ student, changeColor }}>
+        <Home />
+      </StudentContext.Provider>
     </div>
   );
-}
+};
 
 export default App;
