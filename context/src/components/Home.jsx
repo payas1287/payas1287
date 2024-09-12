d5aee30d6d08f1896380457af24ab5c63986d588
payas <sehzade172041@gmail.com>
2024-09-12 17:16:14 +0300

    WE

---
+++ context/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import OgrenciList from "./OgrenciList";
+
+const Home = () => {
+  return (
+    <div>
+        <h1 style={{textAlign:"center", color: "red"}}>OKUL YÖNETİMİ</h1>
+    </div>
+  )
+}
+
+export default Home
