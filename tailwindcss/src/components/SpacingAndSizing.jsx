8ad39ddaeab0614d2e22ea6f3f667fdbfcee47e6
payas <sehzade172041@gmail.com>
2024-09-12 21:42:05 +0300

    re

--- tailwindcss/src/components/SpacingAndSizing.jsx
+++ tailwindcss/src/components/SpacingAndSizing.jsx
@@ -1,21 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const SpacingAndSizing = () => {
   return (
     <div>
-        <div className='text-center'>
-            <h3 className='text-2xl'>Spacing and Sizing</h3>
-            <button className='bg-blue-500 text-white p-4 m-4 text-center px-4'>Tıkla</button>
-            <button className='bg-blue-500 text-white p-4 m-4 text-center ms-4'>Tıkla</button>
-        </div>
-        <div className='text-center'>
-            <button className='bg-green-400 mx-4 px-4 py-3 border-[3px] rounded-full'>Gönder</button>
-            <button></button>
-            <button></button>
-            <button></button>
-        </div>
+      <div className="text-center">
+        <h3 className="text-2xl">Spacing and Sizing</h3>
+        <button className="bg-blue-500 text-white p-4 m-4 text-center px-4">
+          Tıkla
+        </button>
+        <button className="bg-blue-500 text-white p-4 m-4 text-center ms-4">
+          Tıkla
+        </button>
+      </div>
+      <div className="text-center">
+        <button className="bg-green-400 mx-4 px-4 py-3 border-[3px] rounded-full">
+          Gönder
+        </button>
+      </div>
+      <div className="text-center">
+        <button className="bg-indigo-400 py-4 px-4 rounded-xl my-3">Kaydet</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SpacingAndSizing
+export default SpacingAndSizing;
