fb42040d469389b373972d777f43b472d6a0d6a3
payas <sehzade172041@gmail.com>
2024-09-16 12:37:36 +0300

    sf

--- clarus-store/src/pages/Login.jsx
+++ clarus-store/src/pages/Login.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Login = () => {
   return (
     <div className='loginDiv'>
-        <div></div>
+        <div className='h-[500px] w-11/12 sm:w-[475px] bg-white rounded-[20px] p-5'>
+
+        </div>
     </div>
   )
 }
