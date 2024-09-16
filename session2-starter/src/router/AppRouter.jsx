2ada006b2a53e0cf0baec88b6f40f85cffe11efc
payas <sehzade172041@gmail.com>
2024-09-16 19:05:34 +0300

    we

---
+++ session2-starter/src/router/AppRouter.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "../pages/Login";
+import Home from "../pages/Home";
+import Products from "../pages/Products";
+import About from "../pages/About";
+import NotFound from "../pages/NotFound";
+import Navbar from "../components/Navbar";
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Home />} />
+        <Route path="/dashboard/products" element={<Products />} />
+        <Route path="/dashboard/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;
