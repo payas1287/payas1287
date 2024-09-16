4e4fedeb2631f6bee7132476d8f879dcc7f57a0f
payas <sehzade172041@gmail.com>
2024-09-16 20:09:10 +0300

    we

--- clarus-store/src/App.js
+++ clarus-store/src/App.js
@@ -1,14 +1,35 @@
+// import Home from "./pages/Home";
+// import Login from "./pages/Login";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import Products from "./pages/Products";
+// import About from "./pages/About";
+// import NotFound from "./pages/NotFound";
+import AppRouter from "./router/AppRouter";
+import AuthProvider from "./context/AuthProvider";
 
-
-import React from 'react'
-import Login from './pages/Login'
-
-const App = () => {
+function App() {
   return (
-    <div>
-      <Login/>
-    </div>
-  )
+    <AuthProvider>
+      <AppRouter />
+    </AuthProvider>
+    //* route yapısını router dosyasına taşıdık
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Login />} />
+    //     <Route path="/home" element={<Home />} />
+    //     <Route path="/products" element={<Products />} />
+    //     <Route path="/about" element={<About />} />
+    //     <Route path="*" element={<NotFound />} />
+    //   </Routes>
+    // </BrowserRouter>
+    // <div className="min-h-screen bg-slate-700 text-white text-4xl flex justify-center gap-2 items-center">
+    //   <span className="animate-bounce">🏀</span>
+    //   <h1 className="animate-pulse">
+    //     Welcome to Session 1 of the Clarus Store Project FS17!
+    //   </h1>
+    //   <span className="animate-bounce">🏀</span>
+    // </div>
+  );
 }
 
-export default App
+export default App;
