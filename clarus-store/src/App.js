a7596ad623f7bfceb033b69d4d805ca6fb4790bf
payas <sehzade172041@gmail.com>
2024-09-16 13:08:47 +0300

    we

--- clarus-store/src/App.js
+++ clarus-store/src/App.js
@@ -1,17 +1,14 @@
 
 
-function App() {
+import React from 'react'
+import Login from './pages/Login'
+
+const App = () => {
   return (
- 
-    
-    // <div className="min-h-screen bg-slate-700 text-white text-4xl flex justify-center gap-2 items-center">
-    //   <span className="animate-bounce">🏀</span>
-    //   <h1 className="animate-pulse">
-    //     Welcome to Session 1 of the Clarus Store Project FS17!
-    //   </h1>
-    //   <span className="animate-bounce">🏀</span>
-    // </div>
-  );
+    <div>
+      <Login/>
+    </div>
+  )
 }
 
-export default App;
+export default App
--- clarus-store/src/pages/Login.jsx
+++ clarus-store/src/pages/Login.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
-    <div className='loginDiv'>
-        <div className='h-[500px] w-11/12 sm:w-[475px] bg-white rounded-[20px] p-5'>
-
+    <div className="loginDiv">
+      <div className="h-[500px] w-11/12 sm:w-[475px] bg-white rounded-[20px] p-5 flex flex-col justify-center  text-center">
+        <div>
+            <span></span>
+            <h1 className="text-[22px] sm:text.[32px]">Clarus Store </h1>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
