5e17e196879f2dd26fb713ee3e221cca7c51044d
payas <sehzade172041@gmail.com>
2024-09-16 12:35:58 +0300

    wer

--- clarus-store/src/App.js
+++ clarus-store/src/App.js
@@ -3,13 +3,14 @@
 function App() {
   return (
  
-    <div className="min-h-screen bg-slate-700 text-white text-4xl flex justify-center gap-2 items-center">
-      <span className="animate-bounce">🏀</span>
-      <h1 className="animate-pulse">
-        Welcome to Session 1 of the Clarus Store Project FS17!
-      </h1>
-      <span className="animate-bounce">🏀</span>
-    </div>
+    
+    // <div className="min-h-screen bg-slate-700 text-white text-4xl flex justify-center gap-2 items-center">
+    //   <span className="animate-bounce">🏀</span>
+    //   <h1 className="animate-pulse">
+    //     Welcome to Session 1 of the Clarus Store Project FS17!
+    //   </h1>
+    //   <span className="animate-bounce">🏀</span>
+    // </div>
   );
 }
 
---
+++ clarus-store/src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='loginDiv'>
+        <div></div>
+    </div>
+  )
+}
+
+export default Login
