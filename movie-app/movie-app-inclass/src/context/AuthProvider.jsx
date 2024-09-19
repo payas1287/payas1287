7b9fb30c7e9727a302e11afda3e5d8ca249a04db
payas <sehzade172041@gmail.com>
2024-09-19 16:13:26 +0300

    we

--- movie-app/movie-app-inclass/src/context/AuthProvider.jsx
+++ movie-app/movie-app-inclass/src/context/AuthProvider.jsx
@@ -1,92 +1,83 @@
 import {
-    createUserWithEmailAndPassword,
-    onAuthStateChanged,
-    signInWithEmailAndPassword,
-    signOut,
-  } from "firebase/auth";
-  import React, { createContext, useContext, useEffect, useState } from "react";
-  import { auth } from "../auth/firebase";
-  import { useNavigate } from "react-router-dom";
-  import { toastErrorNotify, toastSuccessNotify } from "../helpers/ToastNotify";
-  
-  export const AuthContext = createContext();
-  
-  //* with custom hook
-  export const useAuthContext = () => {
-    return useContext(AuthContext);
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { auth } from "../auth/firebase";
+import { useNavigate } from "react-router-dom";
+import { toastErrorNotify, toastSuccessNotify } from "../helpers/ToastNotify";
+
+export const AuthContext = createContext();
+
+//* with custom hook
+export const useAuthContext = () => {
+  return useContext(AuthContext);
+};
+
+const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    userObserver();
+  }, []);
+
+  const createUser = async (email, password) => {
+    try {
+      //? yeni bir kullanıcı oluşturmak için kullanılan firebase metodu
+      await createUserWithEmailAndPassword(auth, email, password);
+
+      navigate("/");
+      toastSuccessNotify("Registered successfully");
+    } catch (error) {
+      toastErrorNotify(error.message);
+    }
   };
-  
-  const AuthProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(false);
-    const navigate = useNavigate();
-  
-    useEffect(() => {
-      userObserver();
-    }, []);
-  
-    const createUser = async (email, password) => {
-      try {
-        //? yeni bir kullanıcı oluşturmak için kullanılan firebase metodu
-        let userCredential = await createUserWithEmailAndPassword(
-          auth,
-          email,
-          password
-        );
-        console.log(userCredential)
-        navigate("/");
-        toastSuccessNotify("Registered successfully");
-      } catch (error) {
+
+  //* https://console.firebase.google.com/
+  //* => Authentication => sign-in-method => enable Email/password
+  //! Email/password ile girişi enable yap
+  const signIn = async (email, password) => {
+    try {
+      //? mevcut kullanıcının giriş yapması için kullanılan firebase metodu
+      await signInWithEmailAndPassword(auth, email, password);
+
+      navigate("/");
+      toastSuccessNotify("Logged in successfully");
+    } catch (error) {
+      toastErrorNotify(error.message);
+    }
+  };
+
+  const logOut = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        toastSuccessNotify("Logged out successfully");
+      })
+      .catch((error) => {
+        // An error happened.
         toastErrorNotify(error.message);
-      }
-    };
-  
-    //* https://console.firebase.google.com/
-    //* => Authentication => sign-in-method => enable Email/password
-    //! Email/password ile girişi enable yap
-    const signIn = async (email, password) => {
-      try {
-        //? mevcut kullanıcının giriş yapması için kullanılan firebase metodu
-        let userCredential = await signInWithEmailAndPassword(
-          auth,
-          email,
-          password
-        );
-        console.log(userCredential)
-        navigate("/");
-        toastSuccessNotify("Logged in successfully");
-      } catch (error) {
-        toastErrorNotify(error.message);
-      }
-    };
-  
-    const logOut = () => {
-      signOut(auth)
-        .then(() => {
-          // Sign-out successful.
-          toastSuccessNotify("Logged out successfully");
-        })
-        .catch((error) => {
-          // An error happened.
-          toastErrorNotify(error.message);
-        });
-    };
-  
-    const userObserver = () => {
-      //? Kullanıcının signin olup olmadığını takip eden ve kullanıcı değiştiğinde yeni kullanıcıyı response olarak dönen firebase metodu
-      onAuthStateChanged(auth, (user) => {
-        if (user) {
-          const { email, displayName, photoURL } = user;
-          setCurrentUser({ email, displayName, photoURL });
-        } else {
-          // User is signed out
-          setCurrentUser(false);
-        }
       });
-    };
-    console.log(currentUser);
-    const values = { currentUser, createUser, signIn, logOut };
-    return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
   };
-  
-  export default AuthProvider;
-  
+
+  const userObserver = () => {
+    //? Kullanıcının signin olup olmadığını takip eden ve kullanıcı değiştiğinde yeni kullanıcıyı response olarak dönen firebase metodu
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { email, displayName, photoURL } = user;
+        setCurrentUser({ email, displayName, photoURL });
+      } else {
+        // User is signed out
+        setCurrentUser(false);
+      }
+    });
+  };
+  console.log(currentUser);
+  const values = { currentUser, createUser, signIn, logOut };
+  return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
+};
+
+export default AuthProvider;
