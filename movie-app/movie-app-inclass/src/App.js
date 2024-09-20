c82217a96f22568a46a393097904b4f1075f08f6
payas <sehzade172041@gmail.com>
2024-09-20 06:13:47 +0300

    we

--- movie-app/movie-app-inclass/src/App.js
+++ movie-app/movie-app-inclass/src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import AppRouter from "./router/AppRouter";
 import AuthProvider from "./context/AuthProvider";
 import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import MovieProvider from "./context/MovieProvider";
 
 const App = () => {
   return (
-    <div>
+    <div className="dark:bg-gray-dark-main min-h-screen">
       <AuthProvider>
-        <AppRouter />
+        <MovieProvider>
+          <AppRouter />
+        </MovieProvider>
       </AuthProvider>
       <ToastContainer />
     </div>
