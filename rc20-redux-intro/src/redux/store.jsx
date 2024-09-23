91b4e00a3be445d6c6f23ab6d09bb080165517a8
payas <sehzade172041@gmail.com>
2024-09-23 20:29:59 +0300

    we

---
+++ rc20-redux-intro/src/redux/store.jsx
@@ -0,0 +1,12 @@
+import {  combineReducers } from "redux";
+import counterReducer from "./reducers/counterReducer";
+import todoReducer from "./reducers/todoReducer";
+
+
+const topluReducer = combineReducers({
+    counterReducer:counterReducer,
+    todoReducer: todoReducer
+
+})
+
+export const store = createStore(topluReducer)
