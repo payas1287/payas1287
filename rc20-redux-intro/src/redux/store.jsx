991bce6a788c12c88784ff026abff4e9b40fc63d
payas <sehzade172041@gmail.com>
2024-09-23 20:31:21 +0300

    ww

--- rc20-redux-intro/src/redux/store.jsx
+++ rc20-redux-intro/src/redux/store.jsx
@@ -1,4 +1,4 @@
-import {  combineReducers } from "redux";
+import {  combineReducers,legacy_createStore as createStore } from "redux";
 import counterReducer from "./reducers/counterReducer";
 import todoReducer from "./reducers/todoReducer";
 
