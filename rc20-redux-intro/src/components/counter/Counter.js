d46b485b77104d0a40ea99555ee18e79f2755948
payas <sehzade172041@gmail.com>
2024-09-23 20:43:14 +0300

    we

--- rc20-redux-intro/src/components/counter/Counter.js
+++ rc20-redux-intro/src/components/counter/Counter.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import "./counter.css"
 
 const Counter = () => {
   return (
-    <div>Counter</div>
+    <div>
+      <h2>Counter With Redux</h2>
+      <h1> </h1>
+      <h2> </h2>
+      <div>
+        <button></button>
+      </div>
+    </div>
   )
 }
 
