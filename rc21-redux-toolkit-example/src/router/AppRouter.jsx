9cf11552f25419204a2918da8997f32bc4f20d46
payas <sehzade172041@gmail.com>
2024-09-27 07:38:28 +0300

    qw

--- rc21-redux-toolkit-example/src/router/AppRouter.jsx
+++ rc21-redux-toolkit-example/src/router/AppRouter.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import News from "../pages/News";
+import Login from "../pages/Login";
 
 const AppRouter = () => {
   return (
-    <div>AppRouter</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<News />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+
+
+
+
+    </BrowserRouter>
   )
 }
 
