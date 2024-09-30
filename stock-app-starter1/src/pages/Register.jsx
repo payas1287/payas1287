4552cdf90605182b108266b7763759aea74b02f7
payas <sehzade172041@gmail.com>
2024-09-30 21:37:02 +0300

    update

--- stock-app-starter1/src/pages/Register.jsx
+++ stock-app-starter1/src/pages/Register.jsx
@@ -1,109 +1,90 @@
+import Container from "@mui/material/Container"
+import Typography from "@mui/material/Typography"
+import Avatar from "@mui/material/Avatar"
+import LockIcon from "@mui/icons-material/Lock"
+import image from "../assets/result.svg"
+import Grid from "@mui/material/Grid"
 import Box from "@mui/material/Box"
-import Button from "@mui/material/Button"
-import TextField from "@mui/material/TextField"
-import { Form } from "formik"
-import { object, string } from "yup"
+import { Link } from "react-router-dom"
+import RegisterForm, { registerSchema } from "../components/RegisterForm"
+import { Formik } from "formik"
+import useApiRequests from "../services/useApiRequests"
 
-export const registerSchema = object({
-  username: string()
-    .max(20, "Kullanıcı adı 10 karakterden az olmalıdır.")
-    .required("Kullanıcı adı zorunludur"),
-  firstName: string()
-    .max(20, "İsim 20 karakterden az olmalıdır.")
-    .required("İsim zorunludur"),
-  lastName: string()
-    .max(20, "Soyisim 30 karakterden az olmalıdır.")
-    .required("Soyisim zorunludur"),
+const Register = () => {
+  const { register } = useApiRequests()
 
-  email: string()
-    .email("Lütfen geçerli bir email giriniz.")
-    .required("Email zorunludur"),
-  password: string()
-    .required("Şifre zorunludur")
-    .min(8, "Şifre en az 8 karakter olmalıdır")
-    .max(20, "Şifre en fazla 20 karakter olmalıdır")
-    .matches(/\d+/, "Şifre bir sayı içermelidir")
-    .matches(/[a-z]/, "Şifre bir küçük harf içermelidir")
-    .matches(/[A-Z]/, "Şifre bir büyük harf içermelidir")
-    .matches(/[!/[@$!%*?&]+/, "Şifre bir özel karakter içermelidir"),
-})
-
-const RegisterForm = ({
-  values,
-  handleChange,
-  errors,
-  touched,
-  handleBlur,
-}) => {
   return (
-    <Form>
-      <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
-        <TextField
-          label="User Name"
-          name="username"
-          id="userName"
-          type="text"
-          variant="outlined"
-          value={values.username}
-          onChange={handleChange}
-          onBlur={handleBlur}
-          error={touched.username && Boolean(errors.username)}
-          helperText={errors.username}
-        />
-        <TextField
-          label="First Name"
-          name="firstName"
-          id="firstName"
-          type="text"
-          variant="outlined"
-          value={values.firstName}
-          onChange={handleChange}
-          onBlur={handleBlur}
-          error={touched.firstName && Boolean(errors.firstName)}
-          helperText={errors.firstName}
-        />
-        <TextField
-          label="Last Name"
-          name="lastName"
-          id="lastName"
-          type="text"
-          variant="outlined"
-          value={values.lastName}
-          onChange={handleChange}
-          onBlur={handleBlur}
-          error={touched.lastName && Boolean(errors.lastName)}
-          helperText={errors.lastName}
-        />
-        <TextField
-          label="Email"
-          name="email"
-          id="email"
-          type="email"
-          variant="outlined"
-          value={values.email}
-          onChange={handleChange}
-          onBlur={handleBlur}
-          error={touched.email && Boolean(errors.email)}
-          helperText={errors.email}
-        />
-        <TextField
-          label="password"
-          name="password"
-          id="password"
-          type="password"
-          variant="outlined"
-          value={values.password}
-          onChange={handleChange}
-          onBlur={handleBlur}
-          error={touched.password && Boolean(errors.password)}
-          helperText={errors.password}
-        />
-        <Button type="submit" variant="contained" size="large">
-          Submit
-        </Button>
-      </Box>
-    </Form>
+    <Container maxWidth="lg">
+      <Grid
+        container
+        justifyContent="center"
+        direction="row-reverse"
+        rowSpacing={{ sm: 3 }}
+        sx={{
+          height: "100vh",
+          p: 2,
+        }}
+      >
+        <Grid item xs={12}>
+          <Typography variant="h3" color="primary" align="center">
+            STOCK APP
+          </Typography>
+        </Grid>
+
+        <Grid item xs={12} sm={10} md={6}>
+          <Avatar
+            sx={{
+              backgroundColor: "secondary.light",
+              m: "auto",
+              width: 40,
+              height: 40,
+            }}
+          >
+            <LockIcon size="30" />
+          </Avatar>
+          <Typography
+            variant="h4"
+            align="center"
+            mb={2}
+            color="secondary.light"
+          >
+            Register
+          </Typography>
+
+          <Formik
+            initialValues={{
+              username: "",
+              firstName: "",
+              lastName: "",
+              email: "",
+              password: "",
+            }}
+            validationSchema={registerSchema}
+            onSubmit={(values, actions) => {
+              register(values)
+              actions.resetForm()
+              actions.setSubmitting(false)
+            }}
+
+            //? Component prop'u ile Formumuz harici bir component haline getirilebilir.
+            //? bu sayade daha derli toplu bir validayon yapmak mümkün olacaktır.
+            //? Ancak Formik props'larını bu component'e geçmek gerekir.
+            component={(props) => <RegisterForm {...props} />}
+          ></Formik>
+
+          <Box sx={{ textAlign: "center", mt: 2 }}>
+            <Link to="/">Do you have an account?</Link>
+          </Box>
+        </Grid>
+
+        <Grid item xs={0} sm={7} md={6}>
+          <Container>
+            <img src={image} alt="" />
+          </Container>
+        </Grid>
+      </Grid>
+    </Container>
   )
 }
 
-export default RegisterForm
+export default Register
