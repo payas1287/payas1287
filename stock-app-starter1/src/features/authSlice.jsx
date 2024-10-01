3ad2b675af94e8cb161813d072c252a94a3e00ea
payas <sehzade172041@gmail.com>
2024-10-01 06:49:29 +0300

    update

--- stock-app-starter1/src/features/authSlice.jsx
+++ stock-app-starter1/src/features/authSlice.jsx
@@ -21,9 +21,10 @@ const authSlice = createSlice({
     //? Register işlemi için ayrı bir action creator fonksiyonun yazılması
     //? Register ile Login arasında bazı farklılıklar olduğu için ayrı bir fonksiyon yazıldı.
     registerSuccess: (state, { payload }) => {
-      state.loading = false
-      state.username = payload.data.username
       state.token = payload.token
+      state.username = payload.data.username
+      state.loading = false
+      
     },
     fetchFail: (state) => {
       state.loading = false
