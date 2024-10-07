72516218b98c23076635585289eae3cd75326532
payas <sehzade172041@gmail.com>
2024-10-07 18:51:11 +0300

    FirmModal update

---
+++ stock-app-inClass1/src/components/FirmModal.jsx
@@ -0,0 +1,98 @@
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Modal from "@mui/material/Modal";
+import TextField from "@mui/material/TextField";
+import useStockRequests from "../services/useStockRequests";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function FirmModal({ open, handleClose, data, setData }) {
+  const { postStock, putStock } = useStockRequests();
+  const handleChange = (e) => {
+    ReadableStreamDefaultReader({ ...data, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (data.id) {
+      putStock("firms", data);
+    } else {
+      postStock("firms", data);
+    }
+    setData({ image: "", address: "", phone: "", name: "" });
+    handleClose();
+  };
+
+  return (
+    <div>
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <Box
+          sx={{ display: "flex", flexDirection: "column", gap: 2 }}
+          component="form"
+          onSubmit={handleSubmit}
+        >
+          <TextField
+            label="Firm Name"
+            name="name"
+            id="name"
+            type="text"
+            variant="outlined"
+            value={data.name}
+            onChange={handleChange}
+            required
+          />
+          <TextField
+            label="Phone"
+            name="phone"
+            id="phone"
+            type="tel"
+            variant="outlined"
+            value={data.phone}
+            onChange={handleChange}
+            required
+          />
+          <TextField
+            label="Address"
+            name="address"
+            id="address"
+            type="text"
+            variant="outlined"
+            value={data.address}
+            onChange={handleChange}
+            required
+          />
+          <TextField
+            label="Image"
+            name="image"
+            id="image"
+            type="url"
+            variant="outlined"
+            value={data.image}
+            onChange={handleChange}
+            required
+          />
+          <Button variant="contained" type="submit">
+            Submit
+          </Button>
+        </Box>
+      </Box>
+    </Modal>
+  </div>
+  );
+}
