10382239c806f10a051b7135d7bceb92ad97b8ce
payas <sehzade172041@gmail.com>
2024-10-10 13:41:40 +0300

    update

--- stock-app-starter1/src/pages/Dashboard.jsx
+++ stock-app-starter1/src/pages/Dashboard.jsx
@@ -1,14 +1,14 @@
-import React from "react"
-import AppBar from "@mui/material/AppBar"
-import Box from "@mui/material/Box"
-import CssBaseline from "@mui/material/CssBaseline"
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
 
-import Toolbar from "@mui/material/Toolbar"
-import Button from "@mui/material/Button"
-import Typography from "@mui/material/Typography"
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 function Dashboard() {
-  const currentUser = true
+  const currentUser = true;
 
   return (
     <Box sx={{ display: "flex" }}>
@@ -22,7 +22,7 @@ function Dashboard() {
         </Toolbar>
       </AppBar>
     </Box>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
