9e8a425021fa54cc68f1be45f1bb3a98f7ef9349
payas <sehzade172041@gmail.com>
2024-10-14 16:34:17 +0300

    update

--- milestone/src/router/PrivateRouter.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom"
-
-const PrivateRouter = () => {
-  const currentUser = true
-
-  return currentUser ? <Outlet /> : <Navigate to="/" />
-}
-
-export default PrivateRouter
