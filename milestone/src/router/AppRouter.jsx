1df6f2e13cb2a7846db511290366730219f1d941
payas <sehzade172041@gmail.com>
2024-10-14 13:30:58 +0300

    update

--- milestone/src/router/AppRouter.jsx
+++ milestone/src/router/AppRouter.jsx
@@ -6,6 +6,7 @@ import PrivateRouter from "./PrivateRouter";
 import Dashboard from "../pages/Dashboard";
 import Detail from "../pages/Detail";
 import NewBlog from "../pages/NewBlog"
+import About from "../pages/About"
 const AppRouter = () => {
   return (
     <Router>
