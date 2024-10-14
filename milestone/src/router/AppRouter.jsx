095f559b300bcb9603e0de3833892766306d856a
payas <sehzade172041@gmail.com>
2024-10-14 13:31:27 +0300

    update

--- milestone/src/router/AppRouter.jsx
+++ milestone/src/router/AppRouter.jsx
@@ -5,14 +5,14 @@ import Register from "../pages/Register";
 import PrivateRouter from "./PrivateRouter";
 import Dashboard from "../pages/Dashboard";
 import Detail from "../pages/Detail";
-import NewBlog from "../pages/NewBlog"
-import About from "../pages/About"
+import NewBlog from "../pages/NewBlog";
+import About from "../pages/About";
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
-        <Route path="about" element={<About/>} />
-        <Route path="newblog" element={<NewBlog/>} />
+        <Route path="about" element={<About />} />
+        <Route path="newblog" element={<NewBlog />} />
         <Route path="/" element={<Dashboard />} />
         <Route oatah="login" element={<Login />} />
         <Route path="register" element={<Register />} />
