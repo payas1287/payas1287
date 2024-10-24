32c63640341c331aaa0ac3db69e6a67c8b9a6cad
payas <sehzade172041@gmail.com>
2024-10-24 19:00:43 +0300

    update

--- filmapp/src/app/(private)/profile/page.jsx
+++ filmapp/src/app/(private)/profile/page.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
 
+import React from "react";
+export const metadata = {
+  title: "Profile Page",
+};
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+  return <div>Profile</div>;
+};
 
-export default Profile
+export default Profile;
